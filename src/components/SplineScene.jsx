e817bc115c7728673f7722628d88ec1c36603200
Shan Usmani <takhiro79@gmail.com>
2026-07-03 19:41:59 +0530

    fix: loading spinner while Spline loads, fade-in

--- src/components/SplineScene.jsx
+++ src/components/SplineScene.jsx
@@ -1,13 +1,24 @@
+import { useState } from 'react'
 import Spline from '@splinetool/react-spline'
 
 export default function SplineScene() {
+  const [loaded, setLoaded] = useState(false)
+
   return (
     <div className="spline-wrapper">
+      {!loaded && (
+        <div className="spline-loader">
+          <div className="spline-loader__ring" />
+          <span className="spline-loader__text">Loading Scene</span>
+        </div>
+      )}
       <Spline
         scene="https://prod.spline.design/hakiE0znZQxXtQFT/scene.splinecode"
         onLoad={(spline) => {
           spline.setZoom(0.8)
+          setLoaded(true)
         }}
+        style={{ opacity: loaded ? 1 : 0, transition: 'opacity 0.6s ease' }}
       />
     </div>
   )
